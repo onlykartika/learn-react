bda83ac164d1af26725af8481b2192769a89c707
onlykartika <kartika10112@gmail.com>
2024-02-05 21:43:01 +0700

    fundamental part 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
-function Greeting() {
-  return <h2>My First Component</h2>;
+const BookList = () => {
+  return <section className='booklist'>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>
 }
+
+const Book = () => {
+  return <article className='book'>
+    <Image/>
+    <Title/>
+    <Author/>
+
+  </article>
+}
+
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71aNoZYvEhL._AC_UL600_SR600,400_.jpg" alt="Good Energy"></img>
+const Title = () => <h2>Good Energy</h2>
+const Author = () => { return <h4>Casey Means</h4> }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
